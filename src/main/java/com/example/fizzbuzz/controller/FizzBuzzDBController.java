94f4d6ecce2764fd4a56ff85ac065560287f7d4e
Nika Avalishvili <nika.avalishvili@solveva.com>
2022-10-04 14:00:20 +0400

    Remove unused parameter from connectionAndExecute method

--- src/main/java/com/example/fizzbuzz/controller/FizzBuzzDBController.java
+++ src/main/java/com/example/fizzbuzz/controller/FizzBuzzDBController.java
@@ -22,7 +22,7 @@ public class FizzBuzzDBController {
     }
 
 
-    public static void connectionAndExecute(String query, Integer myNumber){
+    public static void connectionAndExecute(String query){
         Connection connection = null;
         Statement statement = null;
 
@@ -40,28 +40,28 @@ public class FizzBuzzDBController {
     @GetMapping("/insertValue")
     public static void addValues(@RequestParam(value = "id") Long id, @RequestParam (value = "myNumber") Integer myNumber, @RequestParam (value = "name") String name) {
         String query = "INSERT INTO fizz_buzzdb (id, my_number, name, result) VALUES ( '" +id+"' , '" +myNumber+"' , '"+name+"', '"+setResult(myNumber)+"')";
-        connectionAndExecute(query, myNumber);
+        connectionAndExecute(query);
         System.out.println("Value Inserted Successfully!");
     }
 
     @GetMapping("/updateValue")
     public static void updateValues(@RequestParam (value = "id") Long id, @RequestParam (value = "myNumber") Integer myNumber, @RequestParam (value = "name") String name) {
         String query = "UPDATE fizz_buzzdb SET  my_number='" +myNumber+"' , name='"+name+"', result='"+setResult(myNumber)+"' WHERE id ='" +id+ "'";
-        connectionAndExecute(query, myNumber);
+        connectionAndExecute(query);
         System.out.println("Value Updated Successfully!");
     }
 
     @GetMapping("/deleteValue")
     public static void deleteValues(@RequestParam (value = "id") Long id) {
         String query = "DELETE FROM fizz_buzzdb WHERE id ='" +id+ "'";
-        connectionAndExecute(query,0);
+        connectionAndExecute(query);
         System.out.println("Value Removed Successfully!");
     }
 
     @GetMapping("/createTable")
     public void createTable(@RequestParam(value = "tableName") String tbl_name, @RequestParam(value = "param1") String name1, @RequestParam(value = "param2") String name2) {
         String query = "CREATE TABLE IF NOT EXISTS "+tbl_name+" (id SERIAL PRIMARY KEY, "+name1+" VARCHAR(255), "+name2+" VARCHAR(255))";
-        connectionAndExecute(query, 0);
+        connectionAndExecute(query);
         System.out.println("Table Created Successfully!");
     }
 }
