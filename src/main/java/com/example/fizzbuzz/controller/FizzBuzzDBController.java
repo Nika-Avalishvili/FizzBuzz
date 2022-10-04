0c65fbc2b42b7586b6dba4fc802807257971987c
Nika Avalishvili <nika.avalishvili@solveva.com>
2022-10-04 13:18:20 +0400

    Make code cleaner

--- src/main/java/com/example/fizzbuzz/controller/FizzBuzzDBController.java
+++ src/main/java/com/example/fizzbuzz/controller/FizzBuzzDBController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.sql.Connection;
 import java.sql.Statement;
 
-
 @RestController
 public class FizzBuzzDBController {
 
-    public static String setResult(Integer myNumber, String result){
+    public static String setResult(Integer myNumber){
+        String result = "";
         if (myNumber % 3 == 0){
             result += "Fizz";
         }
@@ -22,90 +22,46 @@ public class FizzBuzzDBController {
     }
 
 
-    @GetMapping("/insertValue")
-    public static void addValues(@RequestParam(value = "id") Long id, @RequestParam (value = "myNumber") Integer myNumber, @RequestParam (value = "name") String name) {
-
+    public static void connectionAndExecute(String query, Integer myNumber){
         Connection connection = null;
         Statement statement = null;
 
         ConnectFizzBuzzDB objConnectFizzBuzzDB= new ConnectFizzBuzzDB();
         connection = objConnectFizzBuzzDB.get_connection();
 
-
-        String result = "";
-        result = setResult(myNumber, result);
-
         try {
-            String query = "INSERT INTO fizz_buzzdb (id, my_number, name, result) VALUES ( '" +id+"' , '" +myNumber+"' , '"+name+"', '"+result+"')";
             statement = connection.createStatement();
             statement.executeUpdate(query);
-            System.out.println("Value Inserted Successfully!");
-
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @GetMapping("/insertValue")
+    public static void addValues(@RequestParam(value = "id") Long id, @RequestParam (value = "myNumber") Integer myNumber, @RequestParam (value = "name") String name) {
+        String query = "INSERT INTO fizz_buzzdb (id, my_number, name, result) VALUES ( '" +id+"' , '" +myNumber+"' , '"+name+"', '"+setResult(myNumber)+"')";
+        connectionAndExecute(query, myNumber);
+        System.out.println("Value Inserted Successfully!");
     }
 
     @GetMapping("/updateValue")
     public static void updateValues(@RequestParam (value = "id") Long id, @RequestParam (value = "myNumber") Integer myNumber, @RequestParam (value = "name") String name) {
-
-        Connection connection = null;
-        Statement statement = null;
-
-        ConnectFizzBuzzDB objConnectFizzBuzzDB= new ConnectFizzBuzzDB();
-        connection = objConnectFizzBuzzDB.get_connection();
-
-        String result = "";
-        result = setResult(myNumber, result);
-
-        try {
-            String query = "UPDATE fizz_buzzdb SET  my_number='" +myNumber+"' , name='"+name+"', result='"+result+"' WHERE id ='" +id+ "'";
-            statement = connection.createStatement();
-            statement.executeUpdate(query);
-            System.out.println("Value Updated Successfully!");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String query = "UPDATE fizz_buzzdb SET  my_number='" +myNumber+"' , name='"+name+"', result='"+setResult(myNumber)+"' WHERE id ='" +id+ "'";
+        connectionAndExecute(query, myNumber);
+        System.out.println("Value Updated Successfully!");
     }
-
 
     @GetMapping("/deleteValue")
     public static void deleteValues(@RequestParam (value = "id") Long id) {
-
-        Connection connection = null;
-        Statement statement = null;
-
-        ConnectFizzBuzzDB objConnectFizzBuzzDB= new ConnectFizzBuzzDB();
-        connection = objConnectFizzBuzzDB.get_connection();
-
-        try {
-            String query = "DELETE FROM fizz_buzzdb WHERE id ='" +id+ "'";
-            statement = connection.createStatement();
-            statement.executeUpdate(query);
-            System.out.println("Value Removed Successfully!");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String query = "DELETE FROM fizz_buzzdb WHERE id ='" +id+ "'";
+        connectionAndExecute(query,0);
+        System.out.println("Value Removed Successfully!");
     }
 
-
-    //    Test method used to test creation of new tables in PostgreSQL database;
     @GetMapping("/createTable")
     public void createTable(@RequestParam(value = "tableName") String tbl_name, @RequestParam(value = "param1") String name1, @RequestParam(value = "param2") String name2) {
-        Connection connection = null;
-        Statement statement = null;
-        ConnectFizzBuzzDB objConnectFizzBuzzDB= new ConnectFizzBuzzDB();
-        connection = objConnectFizzBuzzDB.get_connection();
-
-        try {
-            String query = "CREATE TABLE IF NOT EXISTS "+tbl_name+" (id SERIAL PRIMARY KEY, "+name1+" VARCHAR(255), "+name2+" VARCHAR(255))";
-            statement = connection.createStatement();
-            statement.executeUpdate(query);
-            System.out.println("Table Created Successfully!");
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        String query = "CREATE TABLE IF NOT EXISTS "+tbl_name+" (id SERIAL PRIMARY KEY, "+name1+" VARCHAR(255), "+name2+" VARCHAR(255))";
+        connectionAndExecute(query, 0);
+        System.out.println("Table Created Successfully!");
     }
 }
